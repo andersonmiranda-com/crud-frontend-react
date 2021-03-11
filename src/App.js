321085239f425fb89aa6a51c8833b1ae0a01efd7
Anderson Miranda <me@andersonmiranda.com>
2021-03-11 18:48:59 +0100

    added components and services

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
 import React, { Component } from "react"
+import {
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css"
+import TutorialsList from "./components/tutorials-list.component"
+import Tutorial from "./components/tutorial.component"
+import AddTutorial from "./components/add-tutorial.component"
 
 class App extends Component {
   render() {
-    return <h1>Hello!!!</h1>
+    return (
+      <div>
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
+          <a href="/tutorials" className="navbar-brand">
+            Crud Test
+          </a>
+          <div className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to={"/tutorials"} className="nav-link">
+                Tutorials
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to={"/add"} className="nav-link">
+                Add
+              </Link>
+            </li>
+          </div>
+        </nav>
+
+        <div className="container mt-3">
+          <Switch>
+            <Route exact path={["/", "/tutorials"]} component={TutorialsList} />
+            <Route exact path="/add" component={AddTutorial} />
+            <Route path="/tutorials/:id" component={Tutorial} />
+          </Switch>
+        </div>
+      </div>
+    );
   }
 }
 
